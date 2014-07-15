421f052cf1e8c9391f8faec0137cf14730520eb9
Paul Koerbitz <paul.koerbitz@gmail.com>
2014-07-15 22:15:05 +0200

    Simplify types

--- src/Data/HBCI/Types.hs
+++ src/Data/HBCI/Types.hs
@@ -18,24 +18,23 @@ type MSGValue = [SEGValue]
 data DEType = AN | Bin | Code | Ctr | Cur | DTAUS | Date | Dig | ID | JN | Num | Time | Wrt
            deriving (Eq, Show, Read)
 
-data DEGdef = DEGdef { degNeedsRequestTag :: Bool, degItems :: [DEGItem] }
-            deriving (Eq, Show)
+data DE = DEdef { deName :: T.Text, deType :: DEType, deMinSize :: Int, deMaxSize :: Maybe Int,
+                  deMinNum :: Int, deMaxNum :: Maybe Int, deValids :: Maybe [T.Text] }
+        | DEval DEValue
+        deriving (Eq, Show)
 
-data DEGItem = DE  { deName :: T.Text, deType :: DEType, deMinSize :: Int, deMaxSize :: Maybe Int,
-                     deMinNum :: Int, deMaxNum :: Maybe Int, deValids :: Maybe [T.Text] }
-             | DEG { degName :: T.Text, degMinNum :: Int, degMaxNum :: Maybe Int, degDef :: DEGdef }
-             | DEVal DEValue
+data DEG = DEG { degName :: T.Text, degMinNum :: Int, degMaxNum :: Maybe Int, degItems :: [DE] }
+         deriving (Eq, Show)
+
+data SEGItem = DEItem DE
+             | DEGItem DEG
              deriving (Eq, Show)
 
-data SEGdef = SEGdef { needsRequestTag :: Bool, segItems :: [DEGItem] }
-            deriving (Eq, Show)
+data SEG = SEG { segName :: T.Text, needsRequestTag :: Bool, segItems :: [SEGItem] }
+         deriving (Eq, Show)
 
-data SFItem = SEG { sfiName :: T.Text, sfiMinNum :: Int, sfiMaxNum :: Maybe Int, sfiSegDef :: SEGdef }
-            | SF  { sfiMinNum :: Int, sfiMaxNum :: Maybe Int, sfiSfDef :: SFdef }
-            deriving (Eq, Show)
+data SF = SF { sfMinNum :: Int, sfMaxNum :: Maybe Int, sfItems :: [SEG] }
+        deriving (Eq, Show)
 
-data SFdef = SFdef { sfdefNeedsRequestTag :: Bool, sfItems :: [SFItem] }
-           deriving (Eq, Show)
-
-data MSGdef = MSGdef { msgRequiresSignature :: Bool, msgRequiresEncryption :: Bool, msgItems :: [SFItem] }
-            deriving (Eq, Show)
+data MSG = MSG { msgRequiresSignature :: Bool, msgRequiresEncryption :: Bool, msgItems :: [SF] }
+         deriving (Eq, Show)
