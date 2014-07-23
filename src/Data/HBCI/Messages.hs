ed15243df7c84d6eaee41706449273e0964f5583
Paul Koerbitz <paul.koerbitz@gmail.com>
2014-07-23 21:45:06 +0200

    Move 'fillDe' out of 'fillSegItem' to be able to test it individually

--- src/Data/HBCI/Messages.hs
+++ src/Data/HBCI/Messages.hs
@@ -10,26 +10,27 @@ import           Data.Traversable (traverse)
 
 import           Data.HBCI.Types
 
+fillDe :: M.Map T.Text T.Text -> T.Text -> DE -> Either T.Text DEValue
+fillDe _        _      (DEval v)                     = Right v
+fillDe userVals prefix (DEdef deNm _ _ _ _ _ valids) =
+  let key = if T.null prefix then deNm else prefix <> "." <> deNm
+      mval = M.lookup key userVals
+  in case mval of
+    Nothing -> Left $ "Required key '" <> key <> "' missing in userVals"
+    (Just val) -> if not (isJust valids) || val `elem` (fromJust valids)
+                  then Right (DEStr val)
+                  else Left ("Value '" <> val <> "' for key '" <> key <> "' not in valid values '" <> T.pack (show (fromJust valids)) <> "'")
+
 fillSeg :: M.Map T.Text T.Text -> SEG -> Either T.Text SEGValue
 fillSeg userVals (SEG segNm _ items) = traverse (fillSegItem userVals segNm) items
 
 fillSegItem :: M.Map T.Text T.Text -> T.Text -> SEGItem -> Either T.Text DEGValue
 fillSegItem userVals = go
   where
-    go prefix (DEItem de)                        = (:[]) <$> fillDe prefix de
+    go prefix (DEItem de)                        = (:[]) <$> fillDe userVals prefix de
     go prefix (DEGItem (DEG degnm _ _ degitems)) =
       let newPrefix = if T.null degnm then prefix else prefix <> "." <> degnm
-      in traverse (fillDe newPrefix) degitems
-
-    fillDe _      (DEval v)                     = Right v
-    fillDe prefix (DEdef deNm _ _ _ _ _ valids) =
-      let key = if T.null prefix then deNm else prefix <> "." <> deNm
-          mval = M.lookup key userVals
-      in case mval of
-        Nothing -> Left $ "Required key '" <> key <> "' missing in userVals"
-        (Just val) -> if not (isJust valids) || val `elem` (fromJust valids)
-                      then Right (DEStr val)
-                      else Left ("Value '" <> val <> "' for key '" <> key <> "' not in valid values '" <> T.pack (show (fromJust valids)) <> "'")
+      in traverse (fillDe userVals newPrefix) degitems
 
 fillMsg :: M.Map T.Text T.Text -> MSG -> Either T.Text MSGValue
 fillMsg userVals (MSG _reqSig _reqEnc items) = concat <$> traverse fillSf items
