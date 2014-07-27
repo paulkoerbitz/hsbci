71d598c693900e57bd628b5c94387ee1240482e6
Paul Koerbitz <paul.koerbitz@gmail.com>
2014-07-27 14:35:50 +0200

    Add a first 'default.nix'
    This is a complete hackjob (I basically followed
    https://ocharles.org.uk/blog/posts/2014-02-04-how-i-develop-with-nixos.html
    without understanding too much of it ...), but works on my machine
    (twist fingers).

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ haskellPackages ? (import <nixpkgs> {}).haskellPackages }:
+let
+  inherit (haskellPackages) cabal httpConduit HUnit mtl testFramework testFrameworkHunit
+  testFrameworkQuickcheck2 text vector xml;
+in
+
+cabal.mkDerivation (self: {
+  pname = "hsbci";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    httpConduit HUnit mtl testFramework testFrameworkHunit
+    testFrameworkQuickcheck2 text vector xml
+  ];
+  testDepends = [
+    HUnit mtl testFramework testFrameworkHunit testFrameworkQuickcheck2
+    text vector xml
+  ];
+  meta = {
+    description = "A fast, simple and modular HBCI library for Haskell";
+    platforms = self.ghc.meta.platforms;
+  };
+})
