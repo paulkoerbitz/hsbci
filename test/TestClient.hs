eeb270a1b7a1d72ae20177d5c5419ea1d6b9f5e0
Paul Koerbitz <paul.koerbitz@gmail.com>
2014-07-29 21:26:58 +0200

    Modify test client to work with different banks
    User can input his BLZ, the PinTan version, url are used from the
    properties file and the right hbci version should be taken from the
    xml files

--- test/TestClient.hs
+++ test/TestClient.hs
@@ -10,23 +10,14 @@ import qualified Data.Text.IO as TIO
 import           Network.HTTP.Conduit
 import           System.Exit (exitSuccess, exitFailure)
 
+import           Data.HBCI.Types
 import           Data.HBCI.HbciDef
 import           Data.HBCI.Messages
 import           Data.HBCI.Gen
 import           Data.HBCI.Parser
 
-addr :: String
--- addr = "https://hbci.postbank.de/banking/hbci.do"
-addr = "https://hbci-pintan-by.s-hbci.de/PinTanServlet"
-
--- 76010085=Postbank|Nürnberg, Mittelfr|PBNKDEFF760|24||https://hbci.postbank.de/banking/hbci.do|220|plus|
--- 38250110=Kreissparkasse Euskirchen|Euskirchen|WELADED1EUS|00|i031.rl.s-hbci.de|https://hbci-pintan-rl.s-hbci.de/PinTanServlet|220|plus|
-
-
 msgVals :: M.Map T.Text T.Text
 msgVals = M.fromList [("Idn.country", "280")
-                     -- ,("Idn.blz", "76010085")
-                     ,("Idn.blz", "12030000")
                      ,("BPD", "0")
                      ,("UPD", "0")
                      ,("lang", "0")
@@ -35,16 +26,21 @@ msgVals = M.fromList [("Idn.country", "280")
 
 main :: IO ()
 main = do
-  hbciPlus <- getXml "resources/hbci-plus.xml" >>= return . getMSGfromXML
+  bankProps <- getBankPropsFromFile "resources/blz.properties" >>= either (\err -> TIO.putStrLn err >> exitFailure) return
+  putStrLn "Please enter your BLZ:"
+  blz <- T.pack <$> getLine
+  props <- maybe (TIO.putStrLn ("Unknown BLZ: " <> blz) >> exitFailure) return (M.lookup blz bankProps)
+  hbciPlus <- getXml ("resources/hbci-" <> (T.unpack $ bankPinTanVersion props) <> ".xml") >>= return . getMSGfromXML
   case hbciPlus of
     Left err -> TIO.putStrLn err
     Right defs -> do
       dialogInitAnonDef <- maybe (putStrLn "Error: Can't find 'DialogInitAnon'" >> exitFailure) return (M.lookup "DialogInitAnon" defs)
-      let msg' = gen <$> fillMsg msgVals dialogInitAnonDef
+      let msgVals' = M.insert "Idn.blz" blz msgVals
+          msg' = gen <$> fillMsg msgVals' dialogInitAnonDef
       case msg' of
         Left err -> TIO.putStrLn ("Error: " <> err) >> exitFailure
         Right msg -> do
-          request' <- parseUrl addr
+          request' <- parseUrl (T.unpack $ bankPinTanUrl props)
           let request = request' { method = "POST"
                                  , requestHeaders = ("Content-Type", "application/octet-stream"): requestHeaders request'
                                  , requestBody = RequestBodyBS msg
