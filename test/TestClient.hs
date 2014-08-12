aaf5e92520524ace6106dfb4a9327111c0e088ee
Paul Koerbitz <paul.koerbitz@gmail.com>
2014-08-12 23:08:58 +0200

    Make test client produce more detailed error messages

--- test/TestClient.hs
+++ test/TestClient.hs
@@ -3,6 +3,7 @@ module Main where
 
 import           Control.Applicative ((<$>))
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import           Data.Monoid ((<>))
 import qualified Data.Map as M
 import qualified Data.Text as T
@@ -30,15 +31,15 @@ main = do
   putStrLn "Please enter your BLZ:"
   blz <- T.pack <$> getLine
   props <- maybe (TIO.putStrLn ("Unknown BLZ: " <> blz) >> exitFailure) return (M.lookup blz bankProps)
-  hbciPlus <- getXml ("resources/hbci-" <> (T.unpack $ bankPinTanVersion props) <> ".xml") >>= return . getMSGfromXML
-  case hbciPlus of
+  hbciDef <- getXml ("resources/hbci-" <> (T.unpack $ bankPinTanVersion props) <> ".xml") >>= return . getMSGfromXML
+  case hbciDef of
     Left err -> TIO.putStrLn err
     Right defs -> do
       dialogInitAnonDef <- maybe (putStrLn "Error: Can't find 'DialogInitAnon'" >> exitFailure) return (M.lookup "DialogInitAnon" defs)
       let msgVals' = M.insert "Idn.blz" (DEStr blz) msgVals
           msg' = gen <$> fillMsg msgVals' dialogInitAnonDef
       case msg' of
-        Left err -> TIO.putStrLn ("Error: " <> err) >> exitFailure
+        Left err -> TIO.putStrLn ("ERROR: " <> err) >> exitFailure
         Right msg -> do
           request' <- parseUrl (T.unpack $ bankPinTanUrl props)
           let request = request' { method = "POST"
@@ -46,4 +47,7 @@ main = do
                                  , requestBody = RequestBodyBS msg
                                  }
           response <- withManager $ httpLbs request
-          putStrLn (show $ responseBody response) >> exitSuccess
+          case (M.lookup "DialogInitAnonRes" defs, parser $ BS.concat $ LBS.toChunks $ responseBody response) of
+            (Nothing     , _ )             -> putStrLn "ERROR: Can't find 'DialogInitAnonRes' in definitions" >> exitFailure
+            (Just _      , Left err)       -> TIO.putStrLn ("ERROR: Can't parse response: " <> err) >> exitFailure
+            (Just diarDef, Right parseRes) -> (putStrLn $ show $ validateAndExtract diarDef parseRes) >> exitSuccess
