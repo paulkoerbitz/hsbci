0d195c4168c2d09df70ffd724e3fa438460f59d4
Paul Koerbitz <paul.koerbitz@gmail.com>
2014-12-17 20:54:36 +0100

    Make the HbciReader, Hbci, HbciIO  a bit simpler to work with

--- src/Data/HBCI/Types.hs
+++ src/Data/HBCI/Types.hs
@@ -10,9 +10,10 @@ import           Text.PrettyPrint
 import           Control.Monad (liftM)
 import           Control.Applicative (Applicative, (<$>))
 import           Control.Monad.Reader (ReaderT, MonadReader, ask, runReaderT)
-import           Control.Monad.State (StateT, MonadState, get, put, runStateT, evalStateT, execStateT)
-import           Control.Monad.Trans.Either (EitherT, hoistEither, left, right)
+import           Control.Monad.State (State, StateT, MonadState, get, put, runStateT, evalStateT, execStateT)
+import           Control.Monad.Trans.Either (EitherT, hoistEither, left, right, runEitherT)
 import           Control.Monad.Trans (MonadIO, lift, liftIO)
+import           Control.Monad.Identity (Identity, runIdentity)
 
 -- FIXME: Make things strict where appropriate
 
@@ -117,40 +118,44 @@ instance HbciPretty MSG where
 
 -- Idea: The HbciState should be run per user and per Bank. That way
 -- we can keep things somewhat localized.
-data BPD = MkBPD { bpdVersion    :: !T.Text
-                 , bpdMaxNumJobs :: !Int
-                 , bpdMsgSize    :: !Int
-                 , bpdOther      :: !(M.Map T.Text DEValue)
-                 } deriving (Eq, Show)
+data BPD = BPD { bpdVersion    :: !T.Text
+               , bpdMaxNumJobs :: !Int
+               , bpdMsgSize    :: !Int
+               , bpdOther      :: !(M.Map T.Text DEValue)
+               } deriving (Eq, Show)
 
-data UPD = MkUPD { updVersion :: !T.Text
-                 } deriving (Eq, Show)
+data UPD = UPD { updVersion :: !T.Text
+               } deriving (Eq, Show)
 
-data HbciConfig = MkHbciConfig { cfgBankProperties :: M.Map T.Text BankProperties
-                               , cfgMessages       :: M.Map T.Text MSG
-                               }
+data HbciUserInfo = HbciUserInfo { uiUserId :: T.Text
+                                 , uiPIN    :: T.Text
+                                 , uiBLZ    :: T.Text
+                                 }
+
+data HbciInfo = HbciInfo { hbciInfoBankProperties :: M.Map T.Text BankProperties
+                         , hbciInfoMessages       :: M.Map T.Text MSG
+                         , hbciInfoUserId         :: T.Text
+                         , hbciInfoPin            :: T.Text
+                         , hbciInfoBlz            :: T.Text
+                         } deriving (Eq, Show)
 
 data TanMode = MkTanMode { tanModeSecfunc :: !T.Text
                          , tanModeName    :: !T.Text
                          , tanModeId      :: !T.Text
                          } deriving (Eq, Show)
 
-data HbciState = MkHbciState { hbciStateBPD      :: !(Maybe BPD)
-                             , hbciStateUPD      :: !(Maybe UPD)
-                             , hbciStateDialogID :: !T.Text -- FIXME: This should probably be 'Maybe T.Text'
-                             , hbciStateMsgNum   :: !Int
-                             , hbciStateSysId    :: !(Maybe T.Text)
-                             , hbciStateTanModes :: !([TanMode])
-                             }
+data HbciState = HbciState { hbciStateBPD      :: !(Maybe BPD)
+                           , hbciStateUPD      :: !(Maybe UPD)
+                           , hbciStateDialogID :: !T.Text -- FIXME: This should probably be 'Maybe T.Text'
+                           , hbciStateMsgNum   :: !Int
+                           , hbciStateSysId    :: !(Maybe T.Text)
+                           , hbciStateTanModes :: !([TanMode])
+                           } deriving (Eq, Show)
 
 
 initialHbciState :: HbciState
-initialHbciState = MkHbciState Nothing Nothing "0" 1 Nothing []
+initialHbciState = HbciState Nothing Nothing "0" 1 Nothing []
 
-data HbciUserInfo = MkHbciUserInfo { uiUserId :: T.Text
-                                   , uiPIN    :: T.Text
-                                   , uiBLZ    :: T.Text
-                                   }
 
 data HbciError =
   HbciErrorInputData T.Text
@@ -159,48 +164,58 @@ data HbciError =
   deriving Show
 
 
--- FIXME: I might not want to throw the new state away even on error. Then the EitherT
--- transformer must go outside of the state transformer ... I'm not entirely sure about
--- this so I am going to punt on it for now ...
+data HbciInfoInternal = HbciInfoInternal { infoInternalInfo :: HbciInfo, infoInternalState :: HbciState }
 
-newtype Hbci a =
-  Hbci { runHbci' :: ReaderT HbciConfig (StateT HbciState (Either HbciError)) a
-       } deriving (Functor, Applicative, Monad, MonadReader HbciConfig, MonadState HbciState)
+type Hbci' r m a = EitherT HbciError (ReaderT r m) a
 
-runHbci :: HbciConfig -> HbciState -> Hbci a -> Either HbciError (a, HbciState)
-runHbci conf state action = runStateT (runReaderT (runHbci' action) conf) state
+-- newtype HbciReader a =
+--   HbciReader { runHbciReader :: Hbci' HbciConfInternal Identity a
+--              } deriving (Functor, Applicative, Monad, MonadReader HbciConfInternal)
+--
+-- newtype Hbci a =
+--   Hbci { runHbci' :: Hbci' HbciConfig (State HbciState) a
+--        } deriving (Functor, Applicative, Monad, MonadReader HbciConfig, MonadState HbciState)
+--
+-- newtype HbciIO a =
+--   HbciIO { runHbciIO' :: Hbci' HbciConfig (StateT HbciState IO) a
+--          } deriving (Functor, Applicative, Monad, MonadReader HbciConfig, MonadState HbciState, MonadIO)
 
-evalHbci :: HbciConfig -> HbciState -> Hbci b -> Either HbciError b
-evalHbci conf state action = fst <$> runHbci conf state action
+type HbciReader a = Hbci' HbciInfoInternal Identity a
 
-data HbciConfInternal = HbciConfInternal { confConfig :: HbciConfig, confState :: HbciState }
+type Hbci a = Hbci' HbciInfo (State HbciState) a
 
-newtype HbciConf a =
-  HbciConf { runHbciConf :: ReaderT HbciConfInternal (Either HbciError) a
-           } deriving (Functor, Applicative, Monad, MonadReader HbciConfInternal)
+type HbciIO a = Hbci' HbciInfo (StateT HbciState IO) a
 
-askConfig :: MonadReader HbciConfInternal m => m HbciConfig
-askConfig = liftM confConfig $ ask
+runHbci :: HbciInfo -> HbciState -> Hbci a -> (Either HbciError a, HbciState)
+runHbci info state action = runIdentity $! runStateT (runReaderT (runEitherT action) info) state
 
-askState :: MonadReader HbciConfInternal m => m HbciState
-askState = liftM confState $ ask
+evalHbci :: HbciInfo -> HbciState -> Hbci b -> Either HbciError b
+evalHbci info state action = fst $ runHbci info state action
 
-liftConf :: HbciConf a -> Hbci a
-liftConf action = do
-  conf <- ask
+askInfo :: MonadReader HbciInfoInternal m => m HbciInfo
+askInfo = liftM infoInternalInfo $ ask
+
+askState :: MonadReader HbciInfoInternal m => m HbciState
+askState = liftM infoInternalState $ ask
+
+liftReader :: (Monad m, MonadState HbciState m) => HbciReader a -> Hbci' HbciInfo m a
+liftReader action = do
+  info <- ask
   state <- get
-  Hbci $! lift $! lift $! runReaderT (runHbciConf action) (HbciConfInternal conf state)
-
-newtype HbciIO a =
-  HbciIO { runHbciIO :: ReaderT HbciConfig (StateT HbciState (EitherT HbciError IO)) a
-         } deriving (Functor, Applicative, Monad, MonadReader HbciConfig, MonadState HbciState, MonadIO)
+  hoistEither $! runIdentity $! runReaderT (runEitherT action) (HbciInfoInternal info state)
 
 liftHbci :: Hbci a -> HbciIO a
 liftHbci action = do
-  conf <- ask
+  info <- ask
   state <- get
-  case runHbci conf state action of
-    Left err -> HbciIO $! lift $! lift $! left err
-    Right (res, state') -> put state' >> return res
+  let (res, state') = runHbci info state action
+  put state'
+  hoistEither res
 
-type HBCI a = ReaderT HbciConfig (StateT HbciState (EitherT HbciError IO)) a
+runHbciIO :: HbciInfo -> HbciState -> HbciIO a -> IO (Either HbciError a, HbciState)
+runHbciIO info state action = runStateT (runReaderT (runEitherT action) info) state
+
+evalHbciIO :: HbciInfo -> HbciState -> HbciIO a -> IO (Either HbciError a)
+evalHbciIO info state action = evalStateT (runReaderT (runEitherT action) info) state
+
+-- type HBCI a = ReaderT HbciInfo (StateT HbciState (EitherT HbciError IO)) a
